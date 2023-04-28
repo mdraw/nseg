2f58676488e66d3b1538490285b8c2773a9cc49f
mdraw <martin.drawitsch@gmail.com>
2023-04-28 03:35:33 +0200

    tr/val split synth512 data

--- nseg/sandbox/zarrify_synth_gt.py
+++ nseg/sandbox/zarrify_synth_gt.py
@@ -1,48 +1,70 @@
+"""Convert synthetic GT from npy to nseg-compatible zarr"""
+
 import numpy as np
 import zarr
+from pathlib import Path
 
-enable_split = True
+split_z_idx = 384
+chunk_shape = (128, 128, 128)
 
-raw_path = '/home/m4/data/synth/from_h5_em_cfw_1.0_full_512_miki.npy'
-lab_path = '/home/m4/data/synth/cc3d_labels_with_prob_512.npy'
-zpath = '/home/m4/data/synth/synth512.zarr'
+# Optionally split into 2 cubes (tr, val)
+splits = [None]
+# splits = [None, 'tr', 'val']
 
-zgroup_paths = [
-    'volumes/raw',
-    'volumes/labels/neuron_ids',
-    'volumes/labels/labels_mask',
-]
+# raw_path = Path('/home/m4/data/synth/from_h5_em_cfw_1.0_full_512_miki.npy')
+# lab_path = Path('/home/m4/data/synth/cc3d_labels_with_prob_512.npy')
+# zarr_out_path = Path('/home/m4/data/synth/synth512.zarr')
 
-# Synthetic cubes are fully labeled -> zero offset, same res for all subvolumes
-shared_attrs_dict = {'offset': [0, 0, 0], 'resolution': [20, 9, 9]}
+raw_path = Path('/home/m4/data/synth/from_h5_em_cfw_1.0_full_512_miki.npy')
+lab_path = Path('/home/m4/data/synth/cc3d_labels_with_prob_512.npy')
+zarr_out_path = Path('/home/m4/data/synth/synth512.zarr')
 
-raw_f32 = np.load(raw_path)
-lab_u32 = np.load(lab_path)
+for split in splits:
 
-raw_rescaled = (raw_f32 / 2. + 0.5) * 255  # [-1, 1] -> [0, 255]
-raw_u8 = raw_rescaled.astype(np.uint8)
-assert raw_u8.ndim == 5
-raw_u8 = raw_u8[0, 0]  # -> 3D
+    if split is not None:
+        zpath = zarr_out_path.with_stem(f'{zpath.stem}_{split}')
+    else:
+        zpath = zarr_out_path
 
-lab_i64 = lab_u32.astype(np.int64)
+    print(f'Creating {zpath}')
 
-# Synthetic cubes are fully labeled -> nothing to be masked out
-lab_mask_u8 = np.ones_like(lab_i64, dtype=np.uint8)
+    # Synthetic cubes are fully labeled -> zero offset, same res for all subvolumes
+    shared_attrs_dict = {'offset': [0, 0, 0], 'resolution': [20, 9, 9]}
 
-chunk_shape = (256, 256, 256)
+    raw_f32 = np.load(raw_path)
+    lab_u32 = np.load(lab_path)
 
-zstore = zarr.DirectoryStore(zpath)
-zroot = zarr.group(store=zstore, overwrite=True)
-# zroot.create_groups(*zgroup_paths)
+    raw_rescaled = (raw_f32 / 2. + 0.5) * 255  # [-1, 1] -> [0, 255]
+    raw_u8 = raw_rescaled.astype(np.uint8)
+    assert raw_u8.ndim == 5
+    raw_u8 = raw_u8[0, 0]  # -> 3D
 
-zgroups = {
-    'volumes/raw': raw_u8,
-    'volumes/labels/neuron_ids': lab_i64,
-    'volumes/labels/labels_mask': lab_mask_u8,
-}
+    lab_i64 = lab_u32.astype(np.int64)
 
-for key, arr in zgroups.items():
-    zroot[key] = arr
-    zroot[key].attrs.put(shared_attrs_dict)
+    # Synthetic cubes are fully labeled -> nothing to be masked out
+    lab_mask_u8 = np.ones_like(lab_i64, dtype=np.uint8)
 
-print('Done')
+
+    zstore = zarr.DirectoryStore(zpath)
+    zroot = zarr.group(store=zstore, overwrite=True)
+
+
+    if split == 'tr':
+        raw_u8 = raw_u8[:split_z_idx, :, :]
+        lab_i64 = lab_i64[:split_z_idx, :, :]
+        lab_mask_u8 = lab_mask_u8[:split_z_idx, :, :]
+
+    elif split == 'val':
+        raw_u8 = raw_u8[split_z_idx:, :, :]
+        lab_i64 = lab_i64[split_z_idx:, :, :]
+        lab_mask_u8 = lab_mask_u8[split_z_idx:, :, :]
+
+    zgroups = {
+        'volumes/raw': raw_u8,
+        'volumes/labels/neuron_ids': lab_i64,
+        'volumes/labels/labels_mask': lab_mask_u8,
+    }
+
+    for key, arr in zgroups.items():
+        zroot[key] = arr
+        zroot[key].attrs.put(shared_attrs_dict)
