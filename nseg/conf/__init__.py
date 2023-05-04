62e1549ce5ac2260c31fb2f6094a972d95b8f932
mdraw <martin.drawitsch@gmail.com>
2023-05-04 17:39:16 +0200

    Fix shortname re-randomization bug
    Short random names were re-randomized on every config node interpolation
    
    TIL OmegaConf resolvers perform node interpolation before value
    resolution by default.

--- nseg/conf/__init__.py
+++ nseg/conf/__init__.py
@@ -5,5 +5,5 @@ Import OmegaConf from this module to get custom resolvers
 import randomname
 from omegaconf import OmegaConf, DictConfig
 
-
-OmegaConf.register_new_resolver('randomname', randomname.get_name)
+# use_cache=True is necessary for node interpolation - otherwise the name is randomized on each access
+OmegaConf.register_new_resolver('randomname', randomname.get_name, use_cache=True)
